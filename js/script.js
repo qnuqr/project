b27953ff3c54e7dfe720fab45ad7d27ab3375a37
Anuar <example@gmail.com>
2021-06-21 17:45:24 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+console.log('arr' + ' - object')
+
+console.log(2 + 2 * 2 != 8)
+
+const isChecked = true,
+      isClose = false;
+      
+console.log(!isChecked || isClose); 
+
+ 
