168d9cd89e82b89a08bf054d5014493444749cc4
Anuar <example@gmail.com>
2021-06-24 18:07:36 +0600

    din_tipizacyia_lsn_26

--- script.js
+++ script.js
@@ -1,78 +1,46 @@
 "use strict";
-
-let numberOfFilms;
-
-
-
-const personalMovieDB = {
-    count: 0,
-    movies:{},
-    actors: {},
-    genres:[],
-    privat: false, 
-    start: function() {
-        personalMovieDB.count = +prompt('сколько фильмов ты посмотрел?', '');
-   
-        while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
-            personalMovieDB.count = +prompt('сколько фильмов ты посмотрел?', '');
-        }
-   }, 
-   rememberMyFilms: function() {
-    for (let i  = 0; i < 2; i++) {
-        const a = prompt('один из последних просмотренных фильмов?', ''),
-              b = prompt('На сколько оцените его?', '' );
-    
-              if(a != null && b != null && a != '' && b != '' && a.length < 50) {
-                personalMovieDB.movies[a] = b;
-                console.log('done');
-             } else {
-                 console.log('error');
-                 i--;
-             }    
-         }
-    },
-    detectPersonalLevel:function() {
-        if(personalMovieDB.count < 10) {
-            console.log('Просмотрено довольно мало фильмов');
-        } else if(personalMovieDB.count >= 10 && personalMovieDB < 30) {
-            console.log('Вы классический зритель');
-        } else if(personalMovieDB.count >= 30) {
-            console.log('Вы киноман');
-        } else {
-            console.log('Error')
-        }
-    },
-    showMyDB: function(hidden) {
-        if (!hidden) {
-            console.log(personalMovieDB);
-        }
-    },
-
-    toggleVisibleMyDB: function() {
-        if (personalMovieDB.privat) {
-            personalMovieDB.privat = false;   
-        } else {
-            personalMovieDB.privat = true;   
-        } 
-    },
-
-    writeYourGenres: function() {
-        for(let i = 1; i <= 3; i++) {
-            let genre = prompt(`Ваш любимый жанр под номером ${i}`);
-            if(genre === '' || genre == null) {
-               console.log('Вы ввели некоректноые данные или не ввели их вовсе'); 
-               i--;
-            } else {
-                personalMovieDB.genres[i - 1] = genre;
-            }  
-        }
-
-        personalMovieDB.genres.forEach((item, i) => {
-             console.log(`Любимый жанр ${i + 1} - это ${item}`);  
-        });
-    }  
-};
-
-  
+//В строку
+console.log(typeof(String(null)));
+console.log(typeof(String(4)));
 
 
+console.log(typeof(5 + ''));
+
+const num = 5;
+console.log("https://vk.com/catalog/" + num);
+
+
+const fontSize = 26 + 'px';
+
+
+//Что угодно превратить в число
+
+console.log(typeof(Number('4')));
+
+console.log(typeof(+'5'));
+
+console.log(typeof(parseInt("15px", 10)));
+
+
+let answ = +prompt("hello", "");
+
+//Что угодно превратить в boolean
+// 0, '', null, undefined, NaN;
+
+//1)
+let switcher = null;
+
+if(switcher) {
+    console.log('Working');
+}
+
+switcher = 1;
+if(switcher) {
+    console.log('Working');
+}
+
+//2)
+console.log(typeof(Boolean('4')));
+
+//3)
+console.log(typeof(!!"444"));
