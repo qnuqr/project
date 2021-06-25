248773d9a54e5e4c0840b20f37369a0908e02ef4
Anuar <example@gmail.com>
2021-06-25 15:29:17 +0600

    zadachiIzSobesedovaniy_lsn_27

--- script.js
+++ script.js
@@ -1,46 +1,32 @@
 "use strict";
-//В строку
-console.log(typeof(String(null)));
-console.log(typeof(String(4)));
+// let x = 5;
+// alert(++x); //6
+
+// [] + false - null + true
+// console.log([] + false - null + true); //NaN
+
+// let y = 1;
+// let x = y = 2;  //2
+// alert(x);
+
+// console.log([] + 1 + 2); //12
+
+// alert("1"[0]); //1  (обращается по индексу, к первому элементу, в данном случае цифра 1) 
+
+// console.log(2 && 1 && null && 0 && undefined); //null (операция "И" запинается на лжи,(NULL))
+
+// console.log(!!(1 && 2) === (1 && 2)); //false
+
+// alert( null || 2 && 3 || 4); //3 (операция "И" запинается на Правде)
+
+// const a = [1,2,3];
+// const b = [1,2,3];
+// console.log(a == b); //false
+
+// alert(+"Infinity");
 
 
-console.log(typeof(5 + ''));
+// console.log("Ежик" < "яюлоко");
 
-const num = 5;
-console.log("https://vk.com/catalog/" + num);
+// console.log(0 || "" || 2 || undefined || true || false); //true
 
-
-const fontSize = 26 + 'px';
-
-
-//Что угодно превратить в число
-
-console.log(typeof(Number('4')));
-
-console.log(typeof(+'5'));
-
-console.log(typeof(parseInt("15px", 10)));
-
-
-let answ = +prompt("hello", "");
-
-//Что угодно превратить в boolean
-// 0, '', null, undefined, NaN;
-
-//1)
-let switcher = null;
-
-if(switcher) {
-    console.log('Working');
-}
-
-switcher = 1;
-if(switcher) {
-    console.log('Working');
-}
-
-//2)
-console.log(typeof(Boolean('4')));
-
-//3)
-console.log(typeof(!!"444"));
