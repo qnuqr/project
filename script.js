e72e6f4daa7427ee49817e8d717c32b9e494a2f1
Anuar <example@gmail.com>
2021-06-25 17:18:28 +0600

    getElementByPage_lsn_28

--- script.js
+++ script.js
@@ -1,32 +1,18 @@
 "use strict";
-// let x = 5;
-// alert(++x); //6
+const box = document.getElementById('box');
+console.log(box);
 
-// [] + false - null + true
-// console.log([] + false - null + true); //NaN
+const btns = document.getElementsByTagName('button');
 
-// let y = 1;
-// let x = y = 2;  //2
-// alert(x);
+console.log(btns[3]);
 
-// console.log([] + 1 + 2); //12
+const circles = document.getElementsByClassName('circle');
+console.log(circles);
 
-// alert("1"[0]); //1  (обращается по индексу, к первому элементу, в данном случае цифра 1) 
+const hearts = document.querySelectorAll('.heart');
+hearts.forEach(item => {
+    console.log(item);
+});
 
-// console.log(2 && 1 && null && 0 && undefined); //null (операция "И" запинается на лжи,(NULL))
-
-// console.log(!!(1 && 2) === (1 && 2)); //false
-
-// alert( null || 2 && 3 || 4); //3 (операция "И" запинается на Правде)
-
-// const a = [1,2,3];
-// const b = [1,2,3];
-// console.log(a == b); //false
-
-// alert(+"Infinity");
-
-
-// console.log("Ежик" < "яюлоко");
-
-// console.log(0 || "" || 2 || undefined || true || false); //true
-
+const oneHeart = document.querySelector('.heart');
+console.log(oneHeart);
