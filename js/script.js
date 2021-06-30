a7ea3144fe516ced133c4b70254cba0aaf63cc6d
Anuar <example@gmail.com>
2021-06-30 15:51:28 +0600

    lsn_39

--- js/script.js
+++ js/script.js
@@ -1,30 +1,39 @@
-'use strict';
-
-const btns = document.querySelectorAll('button'),
-      wrapper = document.querySelector('.btn-block');
-
-// console.log(btns[0].classList.length);
-// console.log(btns[0].classList.item(1));
-// console.log(btns[1].classList.add('red'));
-// // console.log(btns[0].classList.remove('blue'));
-// // console.log(btns[0].classList.toggle('blue'));
+const btn = document.querySelector('.btn');
+let timerId,
+    i = 0;
 
 
-// if (btns[1].classList.contains('red')) {
-//     console.log('red');
+function myAnimation() {
+    const elem = document.querySelector('.box');
+    let pos = 0;
+
+    const id = setInterval(frame, 10);
+    function frame() {
+        if(pos == 300) {
+            clearInterval(id);
+        } else {
+           pos++;
+           elem.style.top = pos + "px";
+           elem.style.left = pos + 'px';
+        }
+    }
+}
+
+btn.addEventListener('click', myAnimation);
+
+
+
+
+// function logger () {
+//     if (i === 3) {
+//         clearInterval(timerId);
+//     }
+//     console.log('text');
+//     i++;
 // }
 
-btns[0].addEventListener('click', () => {
-    // if (!btns[1].classList.contains('red')) {
-    //     btns[1].classList.add('red');
-    // } else {
-    //     btns[1].classList.remove('red');
-    // }
-    btns[1].classList.toggle('red'); 
-});
+// let id = setTimeout(function log(){
+//     console.log('Salem!');
+//     id = setTimeout(log, 500);
+// }, 500);
 
-wrapper.addEventListener('click', (event) => {
-    if (event.target && event.target.classList.contains('blue')) {
-        console.log('Hello');
-    }
-});
