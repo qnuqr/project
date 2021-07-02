d1498eea4c8345e63ba86afbb1869c070f6716f7
Anuar <example@gmail.com>
2021-07-02 15:41:34 +0600

    lsn_46_kontekst_this

--- js/script.js
+++ js/script.js
@@ -1,26 +1,55 @@
 'use strict';
+// 1) Обычная функция: this = window, но если use strict - undefined!!!  ******************************************************
+// function showThis(a, b) {
+//     console.log(this);
+//     function sum() {
+//         console.log(this);
+//         return a + b;
+//     }
+//     console.log(sum());
+// }
+// showThis(8, 9);
 
-function User (name, id) {
-    this.name = name;
-    this.id = id;
-    this.human = true;
-    this.hello = function() {
-        console.log(`Salem ${this.name}`);
-    };
-}
 
-User.prototype.exit = function() {
-    console.log(`Пользоваетль ${this.name} ушел`);
+
+// 2) Контекст у методов объекта - сам объект
+// const obj = {
+//     a: 20,
+//     b: 15,
+//     sum: function () {
+//         function shout() {
+//             console.log(this);
+//         } 
+//         shout();
+//     }
+    
+
+// };
+// obj.sum();
+
+// 3) this - в конструкторах и классах - это новый экземпляр объекта
+// function User (name, id) {
+//     this.name = name;
+//     this.id = id;
+//     this.human = true;
+//     this.hello = function() {
+//         console.log("Salem!" + this.name);
+//     };
+//  }
+//   let anuar = new User('Anuar', 25);
+
+const btn = document.querySelector('button');
+btn.addEventListener('click', function() {
+    this.style.backgroundColor = 'tomato';
+});
+
+const obj = {
+    num: 5,
+    sayNumber: function() {
+        const say = () => {
+            console.log(this.num);
+        };
+        say();
+    }
 };
-const anuar = new User('Anuar', 25);
-const askar = new User('Askar', 32);
-anuar.exit();
-askar.exit();
 
-
-
-console.log(anuar);
-console.log(askar);
-
-anuar.hello();
-askar.hello();
