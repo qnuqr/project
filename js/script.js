1a975930d088b125c8dda58822b074a1a9e9079f
Anuar <example@gmail.com>
2021-07-02 16:16:31 +0600

    lsn_48_classES6

--- js/script.js
+++ js/script.js
@@ -1,55 +1,36 @@
 'use strict';
-// 1) Обычная функция: this = window, но если use strict - undefined!!!  ******************************************************
-// function showThis(a, b) {
-//     console.log(this);
-//     function sum() {
-//         console.log(this);
-//         return a + b;
-//     }
-//     console.log(sum());
-// }
-// showThis(8, 9);
 
-
-
-// 2) Контекст у методов объекта - сам объект
-// const obj = {
-//     a: 20,
-//     b: 15,
-//     sum: function () {
-//         function shout() {
-//             console.log(this);
-//         } 
-//         shout();
-//     }
-    
-
-// };
-// obj.sum();
-
-// 3) this - в конструкторах и классах - это новый экземпляр объекта
-// function User (name, id) {
-//     this.name = name;
-//     this.id = id;
-//     this.human = true;
-//     this.hello = function() {
-//         console.log("Salem!" + this.name);
-//     };
-//  }
-//   let anuar = new User('Anuar', 25);
-
-const btn = document.querySelector('button');
-btn.addEventListener('click', function() {
-    this.style.backgroundColor = 'tomato';
-});
-
-const obj = {
-    num: 5,
-    sayNumber: function() {
-        const say = () => {
-            console.log(this.num);
-        };
-        say();
+class Rectangle {
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
     }
-};
 
+    calcArea() {
+        return this.height * this.width;
+    }
+}
+
+class ColoredRectangleWithText extends Rectangle {
+    constructor(height, width, text, bgColor) {
+        super(height, width);
+        this.text = text;
+        this.bgColor = bgColor;
+    }
+
+    showMyProps() {
+        console.log(`Текст: ${this.text}, цвет: ${this.bgColor}`);
+    }
+}
+
+const div = new ColoredRectangleWithText(25, 10, 'Salem Alem', 'green');
+
+div.showMyProps();
+console.log(div.calcArea());
+
+
+// const square = new Rectangle(8, 10);
+// const long = new Rectangle(1000, 55);
+
+// console.log(long.calcArea());
+// console.log(square.calcArea());
