364c2e7d73f1ede217f2ecfe1c488d01973b85f9
Anuar <example@gmail.com>
2021-08-03 15:50:02 +0600

    lsn_40_date

--- js/script.js
+++ js/script.js
@@ -1,38 +1,25 @@
 "use strict";
-const btn = document.querySelector('.btn');
-let wait,
-    i = 0;
 
-function myAnimation() {
-    const elem = document.querySelector('.box');
-    let pos = 0;
+const now = new Date();
+// console.log(now.getFullYear());
+// console.log(now.getMinutes());
+// console.log(now.getDay());
+// console.log(now.getDate());
+// console.log(now.getHours());
+// console.log(now.getUTCHours());
 
-    const id = setInterval(frame, 10);
 
-    function frame() {
-        if(pos == 300) {
-            clearInterval(id);
-        } else {
-            pos++;
-            elem.style.top = pos + "px";
-            elem.style.left = pos + "px"; 
-        }
-    }
+// console.log(now.getTimezoneOffset());
+
+
+console.log(now.setHours(45));
+console.log(now);
+let start = new Date();
+
+for (let i = 0; i < 100000; i ++) {
+    let b = i ** 3;
 }
 
-btn.addEventListener('click', myAnimation);
-// btn.addEventListener('click', () => {
-//     // const wait = setTimeout(logger, 3000);
-//     wait = setInterval(logger, 500);
+let end = new Date();
 
-// });
-
-  
-
-// function logger() {
-//     if (i === 8) {
-//         clearInterval(wait);
-//     }
-//     console.log('Sapa');
-//     i++;
-// }
+alert(`Цикл отработал за ${end - start} милисекунд`);
