372d05f830c083ad98db7d01c9e113974b714781
Anuar <example@gmail.com>
2021-08-11 09:31:51 +0600

    lsn_58_npm_jsonServer

--- Food/js/script.js
+++ Food/js/script.js
@@ -326,4 +326,8 @@ window.addEventListener('DOMContentLoaded', function() {
 
     }
 
+    fetch('http://localhost:3000/menu')
+        .then(data => data.json())
+        .then(res => console.log(res));
+
 });
