61b2bcc5e2e962c74456c8163d5e6f0e79c866ec
Anuar <example@gmail.com>
2021-08-16 15:25:10 +0600

    lsn_68_getter_setter

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
 "use strict";
 
-// new RegExp('')
+const person = {
+    name: "Anuar",
+    age: 25,
+
+    get userAge() {
+        return this.age;
+    },
+
+    set userAge(num) {
+        this.age = num;
+    }
+};
+
+console.log(person.userAge = 30);
+
+console.log(person.userAge);
+
